0b1e2985a54e138df1af7ac53490b0e4d36a5df6
Eric Leong <me@ericleong.me>
2017-09-26 02:36:52 -0400

    Initial project creation.

---
+++ app/src/main/java/me/ericleong/bentobox/BentoBoxActivity.java
@@ -0,0 +1,13 @@
+package me.ericleong.bentobox;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BentoBoxActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bento_box);
+    }
+}
