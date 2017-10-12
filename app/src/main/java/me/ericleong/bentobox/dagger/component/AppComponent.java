84cbbc06080d492c2315b96915afae6a7c36d343
Eric Leong <ericleong@tumblr.com>
2017-10-12 15:17:23 -0400

    Add AndroidInjectionModule.

--- app/src/main/java/me/ericleong/bentobox/dagger/component/AppComponent.java
+++ app/src/main/java/me/ericleong/bentobox/dagger/component/AppComponent.java
@@ -1,6 +1,7 @@
 package me.ericleong.bentobox.dagger.component;
 
 import dagger.Component;
+import dagger.android.AndroidInjectionModule;
 import me.ericleong.bentobox.dagger.module.AndroidBindingModule;
 import me.ericleong.bentobox.App;
 import me.ericleong.bentobox.dagger.module.SushiModule;
@@ -9,6 +10,7 @@ import me.ericleong.bentobox.dagger.module.SushiModule;
  * Created by Eric on 9/26/2017.
  */
 @Component(modules = {
+        AndroidInjectionModule.class,
         AndroidBindingModule.class
 })
 public interface AppComponent {
